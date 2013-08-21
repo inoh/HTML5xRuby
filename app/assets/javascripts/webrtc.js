5b12fd59a529312f2af21994b53b5233a37daa27
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2013-08-21 23:54:07 +0900

    multi webrtc

--- app/assets/javascripts/webrtc.js
+++ app/assets/javascripts/webrtc.js
@@ -1,17 +1,97 @@
 var WR = (function ($, window) {
 
+  // Chrome
+  var RTCPeerConnection = webkitRTCPeerConnection;
+
+  // Chrome
+  // var connection = new RTCPeerConnection({ "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] });
+  var servers = null;
+  var connections = {};
+  var getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+
   // 指定したVideo要素にカメラを表示します。
-  function settingVideo (id) {
-    navigator.webkitGetUserMedia({video:true},
-      function (stream) {
-        var video = document.getElementById(id);
-        video.src = webkitURL.createObjectURL(stream);
+  function settingVideo (video) {
+    getUserMedia({video:true}, function (stream) {
+      video.src = webkitURL.createObjectURL(stream);
+      connect(stream);
+    });
+  }
+
+  function iceCallback2(event){
+    if (event.candidate) {
+      pc1.addIceCandidate(new RTCIceCandidate(event.candidate));
+      trace("Remote ICE candidate: \n " + event.candidate.candidate);
+    }
+  }
+
+  function connect (stream) {
+    var pc1 = new RTCPeerConnection(servers);
+    pc1.onicecandidate = function (event){
+      if (event.candidate) {
+        pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
       }
-    );
+    };
+    var pc2 = new RTCPeerConnection(servers);
+    pc2.onicecandidate = function (event){
+      if (event.candidate) {
+        pc1.addIceCandidate(new RTCIceCandidate(event.candidate));
+      }
+    };
+    pc2.onaddstream = function (event){
+      var vid2 = document.getElementById('video02')
+      vid2.src = URL.createObjectURL(event.stream);
+    }; 
+    pc1.addStream(stream);
+    pc1.createOffer(function (desc){
+      pc1.setLocalDescription(desc);
+      pc2.setRemoteDescription(desc);
+      pc2.createAnswer(function (desc){
+          pc2.setLocalDescription(desc);
+          pc1.setRemoteDescription(desc);
+        }, null,
+        {'mandatory': {'OfferToReceiveAudio':true, 'OfferToReceiveVideo':true }}
+      );
+    });
+
+    /*
+    var connection = new RTCPeerConnection(servers);
+    connection.onicecandidate = function (event){
+      if (event.candidate) {
+        pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
+        trace("Local ICE candidate: \n" + event.candidate.candidate);
+      }
+    };
+    return connection;
+    */
+  }
+
+  // connectionの状態をログに出力します。
+  function connectionStateLog (start_message) {
+    if (start_message) {
+      console.log('---> ' + start_message);
+    }
+    // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCIceConnectionState
+    console.log('iceConnectionState : ' + connection.iceConnectionState);
+    // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCIceGatheringState
+    console.log('iceGatheringState : ' + connection.iceGatheringState);
+    // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCSessionDescription
+    console.log('localDescription : ' + connection.localDescription);
+    // onaddstream
+    // onicecandidate
+    // oniceconnectionstatechange
+    // onnegotiationneeded
+    // onremovestream
+    // onsignalingstatechange
+    // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCSessionDescription
+    console.log('remoteDescription : ' + connection.remoteDescription);
+    // http://dev.w3.org/2011/webrtc/editor/webrtc.html#idl-def-RTCSignalingState
+    console.log('signalingState : ' + connection.signalingState);
   }
 
   return {
-    settingVide: settingVideo
+    settingVideo: settingVideo,
+    connect: connect,
+    connectionStateLog: connectionStateLog
   }
 
 })(jQuery, window);
